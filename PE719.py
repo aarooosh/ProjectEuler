be3452376328c49940a17c5ea1bf6e3049de5d8d
A_Ghate <98141133+aarooosh@users.noreply.github.com>
2024-08-12 22:09:48 +0530

    Update PE719.py

--- PE719.py
+++ PE719.py
@@ -1,21 +1,27 @@
-def check(i, m, digits, memo):
-    if i == 0 and len(m) == 0:
+def check(sum, m, digits, memo):
+    # Recursively Makes Partitions of the string , without exceeding the max digit size
+    # Currently this is the slowest step and could use some optimizations
+    if sum == 0 and len(m) == 0:
+        # Sum is zero and all digits used
         return True
-    if len(m) == 0 or i < 0:
+    if len(m) == 0 or sum < 0:
+        # prematurely used up all digits  or sum < 0
         return False
-    if (i, m) in memo:
-        return memo[(i, m)]
+    if (sum, m) in memo:
+        # Checks if the combo is already present in our memory
+        return memo[(sum, m)]
     for j in range(1, digits + 1):
         num = int(m[:j])
-        if check(i - num, m[j:], digits, memo):
-            memo[(i, m)] = True
+        if check(sum - num, m[j:], digits, memo):
+            # checks if sum-num can be represented , because then adding num makes sum representable
+            memo[(sum, m)] = True
             return True
-    memo[(i, m)] = False
+    memo[(sum, m)] = False
     return False
 
 def compute_sum():
     total_sum = 0
-    
+    # Going over only the first 10^6 elements and using their squares 
     for i in range(2, 10000001):
         num = i + k
         squared = num ** 2
@@ -27,3 +33,4 @@ def compute_sum():
     print('sum =', total_sum)
 
 compute_sum()
+# Unfortunately this code takes about 3 hours to run :(
